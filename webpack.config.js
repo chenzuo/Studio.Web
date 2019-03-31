b428b8f8162d50683f4c8a5031782ce212174ed8
abcd@1234 <chenzuo@hotmail.com>
2019-03-31 20:10:54 +0800

    增加webpack打包.运行脚本要在更高的浏览器上

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+module.exports = {
+    mode: 'development',
+
+    entry: './src/index.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: "ts-loader"
+        }]
+    },
+    resolve: {
+        extensions: [
+            '.ts'
+        ]
+    }
+};
